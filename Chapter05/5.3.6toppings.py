cd748172211303937cbfc8c908ccc77473963d7d
OSYouth <benleolee@163.com>
2021-01-27 18:12:40 +0800

    提交最近改变的代码，保持版本同步一下

---
+++ Chapter05/5.3.6toppings.py
@@ -0,0 +1,20 @@
+requested_toppings = ['mushrooms','extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print ("\nFinished making your pizza!")
+
+# 对比 if-elif-else:
+requested_toppings = ['mushrooms','extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print ("\nFinished making your pizza!")
